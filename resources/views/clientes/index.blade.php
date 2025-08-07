19eb8c0bef421a87125be0a8b60bb85aef0f8eae
jeissonzs09 <jeisson.zelaya@unah.hn>
2025-08-06 23:09:27 -0600

    Prueba

--- resources/views/clientes/index.blade.php
+++ resources/views/clientes/index.blade.php
@@ -2,7 +2,7 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="text-xl font-bold flex items-center gap-2">
-            <i class="fas fa-users"></i> Clientes
+            <i class="fas fa-users"></i> Clientes Commit
         </h2>
     </x-slot>
 
